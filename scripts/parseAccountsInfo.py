9718c1507d39c26a84938b8daca0d68eedbcad68
Leonardo Marangoni <leonardomarangoni@hotmail.com>
2017-05-02 12:10:25 -0300

    Implemented Cos mapping

--- scripts/parseAccountsInfo.py
+++ scripts/parseAccountsInfo.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python 
+#!/usr/bin/python
 # encoding=utf8
 
 import sys
@@ -11,8 +11,8 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 parser = argparse.ArgumentParser(
-    description='Authenticates as zimbra administrator and get the number of\
-    \nmessages in Inbox of accounts from user list.',
+    description='Authenticates as zimbra administrator and returns a list\
+    \nof accounts and their attrs(previously defined on zimbraAtributes.',
     formatter_class=RawTextHelpFormatter)
 parser.add_argument('-a', '--admin', action='store', dest='admin',
                     help='Zimbra Admin Account to authenticate on server',
@@ -20,10 +20,8 @@ parser.add_argument('-a', '--admin', action='store', dest='admin',
 parser.add_argument('-p', '--pass', action='store', dest='password',
                     help='Zimbra Admin Password String', required=True)
 parser.add_argument('-d', '--domains', action='store', dest='domains',
-                    help='List of domains to get accounts info\
-                    \n\tExample:\n--domains example.com or\
-                    \n--domains example.net,example.com,exam...,',
-                    required=True)
+                    help='Domain to get accounts info\
+                    \n\tExample: \n--domains example.com', required=True)
 parser.add_argument('-H', '--host', action='store', dest='hostname',
                     help='webmail URL', required=True)
 
@@ -36,9 +34,14 @@ domains = argslist.domains.split(',')
 comm = Communication(url)
 
 
-def zimbraAtributes():
-    return ['cn',
-            'displayName',
+def multipleReplace(text, wordDict):
+    for key in wordDict:
+        text = text.replace(key, wordDict[key])
+    return text
+
+
+def zimbraAttributes():
+    return ['displayName',
             'zimbraAccountStatus',
             'zimbraCOSId',
             'zimbraIsAdminAccount',
@@ -68,28 +71,33 @@ def getAllInfo(url, adm, pword, domain):
 
     response = comm.send_request(request)
     if not response.is_fault():
-        return response.get_response(2)
+        resp = [response.get_response(1), response.get_response(2)]
+        return resp
     else:
         return "Error on zimbraAdmin: " + response.get_fault_message()
 
 
 accountlist = []
 allInfo = getAllInfo(url, admin, password, domains[0])
-accresp = allInfo['GetAllAccountsResponse']['account']
+accresp = allInfo[1]['GetAllAccountsResponse']['account']
+cosresp = allInfo[0]['GetAllCosResponse']['cos']
+cosdict = {}
+
+for cos in cosresp:
+    cosdict[cos['id']] = cos['name']
+
 line = "account"
-for i in zimbraAtributes():
+for i in zimbraAttributes():
     line += ", %s" % (i)
-print (line)
+archive = line
 for account in accresp:
     line = account['name']
     attrs = account['a']
-    for i in zimbraAtributes():
+    for i in zimbraAttributes():
         line += ","
         for x in attrs:
             if i.encode('utf-8') == x.values()[1].encode('utf-8'):
-                line += x.values()[0]
-    print (line)
+                line += multipleReplace(x.values()[0], cosdict)
+    archive += "\n%s" % (line)
 
-# print allInfo
-print (accresp[1].keys())
-# print accresp[1]['a']['displayName']
+print archive
