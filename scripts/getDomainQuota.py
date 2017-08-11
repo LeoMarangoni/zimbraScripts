abfee93151093c2526dfd20d676ea01b2a4d2984
Leonardo marangoni <leonardo.marangoni@inova.net>
2017-08-11 12:11:14 -0300

    Added SendMail to getDomainQuota

--- scripts/getDomainQuota.py
+++ scripts/getDomainQuota.py
@@ -11,6 +11,9 @@ from pythonzimbra.tools import auth
 from pythonzimbra.communication import Communication
 import argparse
 from argparse import RawTextHelpFormatter
+from modules import sendmail
+from datetime import datetime
+
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -29,6 +32,9 @@ parser.add_argument('-d', '--domain', action='store', dest='domain',
                     \n\tExample: \n--domain example.com', required=True)
 parser.add_argument('-H', '--host', action='store', dest='hostname',
                     help='webmail URL', required=True)
+parser.add_argument('-S', '--send-mail', dest='send_mail', nargs='+',
+                    metavar=('email', 'smtp-server'),
+                    help='Add this if you want to send output by email')
 
 argslist = parser.parse_args()
 
@@ -36,9 +42,8 @@ url = "https://" + argslist.hostname + ":7071/service/admin/soap"
 admin = argslist.admin
 password = argslist.password
 domain = argslist.domain
+sendm = argslist.send_mail
 comm = Communication(url)
-
-archive = []
 
 
 def getAllInfo(url, adm, pword, domain):
@@ -60,11 +65,43 @@ def getAllInfo(url, adm, pword, domain):
         return "Error on zimbraAdmin: " + response.get_fault_message()
 
 
-for account in getAllInfo(url, admin, password, domain):
-    archive.append("%s,%i,%i" % (account['name'],
-                                 int(account['used'] / 1024 / 1024),
-                                 int(account['limit'] / 1024 / 1024))
-                   )
+def getDomainQuota():
+    archive = []
+    for account in getAllInfo(url, admin, password, domain):
+        archive.append("%s,%i,%i" % (account['name'],
+                                     int(account['used'] / 1024 / 1024),
+                                     int(account['limit'] / 1024 / 1024))
+                       )
 
-archive = sorted(archive)
-print("\n".join(archive))
+    archive = sorted(archive)
+    return("\n".join(archive))
+
+
+if sendm is not None:
+    try:
+        smtpserver = sendm[1]
+    except IndexError:
+        smtpserver = argslist.hostname
+    finally:
+        today = datetime.now().date()
+        subject = "Quotas - Accounts List %s - %s" % (domain, today)
+        message = "Hello,\nSAttached is the list of accounts\
+                   of the domain %s" % (domain)
+        path = "/tmp/%s.csv" % (domain)
+        csv = open(path, "w")
+        csv.write(str(getDomainQuota()))
+        csv.close()
+        try:
+            sendmail.send_mail(admin,
+                               [sendm[0]],
+                               subject,
+                               message,
+                               server=smtpserver,
+                               user=admin,
+                               passw=password,
+                               files=[path])
+            print "List sent to %s, please check your mailbox" % (sendm[0])
+        except Exception as e:
+            print "%s, try to change smtp server" % e
+else:
+    print str(getDomainQuota())
